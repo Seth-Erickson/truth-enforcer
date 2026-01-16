a75c82b6f7fa2b218848f3b7e38ee5ec3f948920
Seth-Erickson <manifold.tinker389@passmail.com>
2026-01-16 00:51:59 -0600

    Update setup.py
    Removed instructional comments

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="truth-enforcer",
-    version="0.1.1",  # Bumped version to show progress
+    version="0.1.1",  
     description="A Topological Hallucination Detection System for LLMs",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Seth Erickson",  # Matches your GitHub
+    author="Seth Erickson",  
     author_email="Manifold.tinket389@passmail.com",
     url="https://github.com/Seth-Erickson/truth-enforcer",
-    py_modules=["truth_enforcer"], # CRITICAL: Matches the new underscore filename
+    py_modules=["truth_enforcer"], 
     install_requires=[
         "sentence-transformers>=2.2.2",
         "ripser>=0.6.4",
